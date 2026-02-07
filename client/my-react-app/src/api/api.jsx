7803ff488890ee602bacf8ee3dd3d64c9134edb1
Birhanu <birhanumatebe12@gmail.com>
2026-02-08 00:20:31 +0300

    feat: add api.jsx

---
+++ client/my-react-app/src/api/api.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API_BASE = "http://127.0.0.1:5000/api";
+
+
+export const getHistory = () => axios.get(`${API_BASE}/historical-data`);
+export const getPrices = () => axios.get(`${API_BASE}/prices`);
+export const getChangePoint = () => axios.get(`${API_BASE}/change-point`);
+export const getEvents = () => axios.get(`${API_BASE}/events`);
+export const getSummary = () => axios.get(`${API_BASE}/summary`);
