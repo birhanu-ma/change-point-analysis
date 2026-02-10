9428bb5e43489d16d3c43c4dfe885e209f5343d1
Birhanu <birhanumatebe12@gmail.com>
2026-02-10 10:18:09 +0300

    feat: add endpoint

--- client/my-react-app/src/api/api.jsx
+++ client/my-react-app/src/api/api.jsx
@@ -8,3 +8,4 @@ export const getPrices = () => axios.get(`${API_BASE}/prices`);
 export const getChangePoint = () => axios.get(`${API_BASE}/change-point`);
 export const getEvents = () => axios.get(`${API_BASE}/events`);
 export const getSummary = () => axios.get(`${API_BASE}/summary`);
+export const getAllEvent = () => axios.get(`${API_BASE}/events-data`);
